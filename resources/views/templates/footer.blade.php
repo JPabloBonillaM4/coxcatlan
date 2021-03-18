379250644f81f8d837f93c4f07dcd48f484b6f30
Juan Pablo <juan@teknik.mx>
2021-03-17 22:25:52 -0600

    Starting project

---
+++ resources/views/templates/footer.blade.php
@@ -0,0 +1,46 @@
+<!-- Vendor -->
+<script src="{{ asset('vendor/jquery/jquery.min.js?v='.rand()) }}"></script>
+<script src="{{ asset('vendor/jquery.appear/jquery.appear.min.js?v='.rand()) }}"></script>
+<script src="{{ asset('vendor/jquery.easing/jquery.easing.min.js?v='.rand()) }}"></script>
+<script src="{{ asset('vendor/jquery.cookie/jquery.cookie.min.js?v='.rand()) }}"></script>
+<script src="{{ asset('vendor/popper/umd/popper.min.js?v='.rand()) }}"></script>
+<script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js?v='.rand()) }}"></script>
+<script src="{{ asset('vendor/common/common.min.js?v='.rand()) }}"></script>
+<script src="{{ asset('vendor/jquery.validation/jquery.validate.min.js?v='.rand()) }}"></script>
+<script src="{{ asset('vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js?v='.rand())}} "></script>
+<script src="{{ asset('vendor/jquery.gmap/jquery.gmap.min.js?v='.rand()) }}"></script>
+<script src="{{ asset('vendor/jquery.lazyload/jquery.lazyload.min.js?v='.rand()) }}"></script>
+<script src="{{ asset('vendor/isotope/jquery.isotope.min.js?v='.rand()) }}"></script>
+<script src="{{ asset('vendor/owl.carousel/owl.carousel.min.js?v='.rand()) }}"></script>
+<script src="{{ asset('vendor/magnific-popup/jquery.magnific-popup.min.js?v='.rand())}}"></script>
+<script src="{{ asset('vendor/vide/jquery.vide.min.js?v='.rand()) }}"></script>
+<script src="{{ asset('vendor/vivus/vivus.min.js?v='.rand()) }}"></script>
+
+<!-- Theme Base, Components and Settings -->
+<script src="{{ asset('js/theme.js?v='.rand()) }}"></script>
+
+<!-- Current Page Vendor and Views -->
+<script src="{{ asset('vendor/circle-flip-slideshow/js/jquery.flipshow.min.js?v='.rand()) }}"></script>
+<script src="{{ asset('js/views/view.home.js?v='.rand()) }}"></script>
+
+<!-- Theme Custom -->
+<script src="{{ asset('js/custom.js?v='.rand()) }}"></script>
+
+
+<!-- Theme Initialization Files -->
+<script src="{{ asset('js/theme.init.js?v='.rand()) }}"></script>
+
+<!-- Google Analytics: Change UA-XXXXX-X to be your site's ID. Go to http://www.google.com/analytics/ for more information.
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-12345678-1', 'auto');
+    ga('send', 'pageview');
+</script>
+-->
+
+</body>
+</html>
---
+++ resources/views/templates/header.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<!-- Basic -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">	
+
+		<title>@yield('title','Coxcatlan | Inicio')</title>	
+
+		<meta name="keywords" content="HTML5 Template" />
+		<meta name="description" content="Coxcatlan, Puebla">
+		<meta name="author" content="okler.net">
+
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+		<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+
+		<!-- Mobile Metas -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
+
+		<!-- Web Fonts  -->
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800%7CShadows+Into+Light&display=swap" rel="stylesheet" type="text/css">
+
+		<!-- Vendor CSS -->
+		<link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css?v='.rand()) }}">
+		<link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css?v='.rand()) }}">
+		<link rel="stylesheet" href="{{ asset('vendor/animate/animate.compat.css?v='.rand()) }}">
+		<link rel="stylesheet" href="{{ asset('vendor/simple-line-icons/css/simple-line-icons.min.css?v='.rand()) }}">
+		<link rel="stylesheet" href="{{ asset('vendor/owl.carousel/assets/owl.carousel.min.css?v='.rand()) }}">
+		<link rel="stylesheet" href="{{ asset('vendor/owl.carousel/assets/owl.theme.default.min.css?v='.rand()) }}">
+		<link rel="stylesheet" href="{{ asset('vendor/magnific-popup/magnific-popup.min.css?v='.rand()) }}">
+
+		<!-- Theme CSS -->
+		<link rel="stylesheet" href="{{ asset('css/theme.css?v='.rand()) }}">
+		<link rel="stylesheet" href="{{ asset('css/theme-elements.css?v='.rand()) }}">
+		<link rel="stylesheet" href="{{ asset('css/theme-blog.css?v='.rand()) }}">
+		<link rel="stylesheet" href="{{ asset('css/theme-shop.css?v='.rand()) }}">
+
+		<!-- Current Page CSS -->
+		<link rel="stylesheet" href="{{ asset('vendor/circle-flip-slideshow/css/component.css?v='.rand()) }}">
+		<!-- Skin CSS -->
+		<link rel="stylesheet" href="{{ asset('css/skins/default.css?v='.rand()) }}"> 
+
+		<!-- Theme Custom CSS -->
+		<link rel="stylesheet" href="{{ asset('css/custom.css?v='.rand()) }}">
+
+		<!-- Head Libs -->
+		<script src="{{ asset('vendor/modernizr/modernizr.min.js?v='.rand()) }}"></script>
+
+	</head>
+	<body class="loading-overlay-showing" data-plugin-page-transition data-loading-overlay data-plugin-options="{'hideDelay': 500}">
